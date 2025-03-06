6178b0daba83eb268c7d5dcd633a0fe2b6db453b
gomadango0113 <gomadango0113@gmail.com>
2025-03-06 20:40:22 +0900

    first commit

---
+++ src/main/java/org/gomadango0113/player_manager/Main.java
@@ -0,0 +1,17 @@
+package org.gomadango0113.player_manager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
